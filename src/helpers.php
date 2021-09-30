7f15a0d27ede58abf90de70fc3adf5cb7511b8f9
librevlad <librevlad@gmail.com>
2021-09-30 23:26:53 +0300

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,8 @@
 <?php
 
-// any helpers
+
+function chisel_project_data_path( $path = '' ) {
+
+	return rtrim( config( 'chisel.data_path' ) . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR ) . $path;
+
+}
